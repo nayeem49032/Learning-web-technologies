94ecf4154ed512ea3e580b5b9025b6a62d330a5e
nayeem49032 <nayeemmg666@gmail.com>
2026-03-04 22:49:12 +0600

    Pratrice js||learn upto date of birth

--- JS/script.js
+++ JS/script.js
@@ -19,4 +19,51 @@ function name_validation() {
         alert("success")
     }
 }
+let email = document.getElementById('usermail');
+//email.addEventListener('click',email_validation);
+
+let btnmail = document.getElementById('btnemail');
+btnmail.addEventListener('click', email_validation);
+
+function email_validation() {
+
+    if (email.value === "") {
+        alert("Email should be entered");
+    }
+    else if (!/^[a-zA-z0-9.-_]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email.value)) {
+        alert('Input Valid Email');
+    }
+}
+let btngender = document.getElementById('btngen');
+btngender.addEventListener('click', gender_validation);
+
+const genders = document.getElementsByName('Gender');
+function gender_validation() {
+    const genderselected = document.querySelector('input[name="Gender"]:checked');
+    if (!genderselected) {
+        alert('Please select your gender');
+    }
+}
+
+let txtBox_date = document.getElementById('dd');
+let txtBox_month = document.getElementById('mm');
+let txtBox_year = document.getElementById('yyyy');
+let btndate = document.getElementById('datesubmit');
+btndate.addEventListener('click', date_validation);
+
+function date_validation() {
+    if (txtBox_date.value === "" || txtBox_month.value === "" || txtBox_month.value === "") {
+        alert('Please fill up all fields (Date, Month, Year)');
+    }
+    else if (txtBox_date.value < 1 || txtBox_date.value > 31) {
+        alert('Invalid Date (Must be 1-31)');
+    }
+    else if (txtBox_month.value < 1 || txtBox_month.value > 12) {
+        alert('Invalid Month (Must be 1-12)');
+    }
+    else if (txtBox_year.value > 1900 || txtBox_year.value > currentYear) {
+        alert('Invalid Year (Enter a realistic year)');
+    }
+}
+
 console.log("successfully connected....");
